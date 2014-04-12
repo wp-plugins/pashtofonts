c2788f5a0fbba232ce85d612d6b12a344c5ad7ed
moxet <moxet@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-12 15:37:13 +0000

    Shortcode function removed due to security issues.
    git-svn-id: https://plugins.svn.wordpress.org/pashtofonts/trunk@891922 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- PashtoFonts.php
+++ PashtoFonts.php
@@ -113,35 +113,6 @@ function check_english($text) {
 	}
 }
 
-//shortcode display in editor
-add_action('media_buttons','add_sc_select',11);
-function add_sc_select(){
-    global $shortcode_tags;
-     /* ------------------------------------- */
-     /* enter names of shortcode to exclude bellow */
-     /* ------------------------------------- */
-    $exclude = array("pukhto", "pukhto_lek", "english", "english_text");
-    echo "&nbsp;<select id='sc_select'><option>PashtoFonts</option>";
-    foreach ($shortcode_tags as $key => $val){
-            if(in_array($key,$exclude)){
-            $shortcodes_list .= "<option value=\"['.$key.'][/'.$key.']\">'.$key.'</option>";
-            }
-        }
-     echo $shortcodes_list;
-     echo "</select>";
-}
-add_action('admin_head', 'button_js');
-function button_js() {
-        echo "<script type='text/javascript'>
-        jQuery(document).ready(function(){
-           jQuery('#sc_select').change(function() {
-                          send_to_editor(jQuery('#sc_select :selected').val());
-                          return false;
-                });
-        });
-</script>";
-}
-
 //pashto Formatting
 function pashtoFormattingPost($text){
 if (check_pashto($text)) {
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link:https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id
 Tags: Pashto, Urdu, Unicode, Web Fonts, RTL
 Requires at least: 3.0.1
 Tested up to: 3.8.1
-Stable tag: 1.0.1
+Stable tag: 1.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -59,16 +59,10 @@ Yes, to facilitate Urdu bloggers we have an extra font Nafees. If you want to di
 
 == Changelog ==
 
-= 1.0.1 =
-* Shortcut buttons for shortcodes has been added to post editor.
-
 = 1.0 =
 * Initial Release.
 
 == Upgrade Notice ==
-
-= 1.0.1 =
-Its easier to insert shortcodes from editor rather than writing.
 
 = 1.0 =
 Initial Release
