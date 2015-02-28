7c265165569280d7b94d7c38f3dbbfb9bcd3d58b
moxet <moxet@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-28 07:17:36 +0000

    Plugin icons added
    git-svn-id: https://plugins.svn.wordpress.org/pashtofonts/trunk@1101731 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- content.php
+++ content.php
@@ -1,4 +1,4 @@
-	<table border="0" cellpadding="0" cellspacing="0" style="border-collapse: collapse" bordercolor="#111111" width="100%">
+<table border="0" cellpadding="0" cellspacing="0" style="border-collapse: collapse" bordercolor="#111111" width="100%">
   <tr>
     <td valign="top" style="width:40%">
 	<form method="post" name="options" target="_self">
